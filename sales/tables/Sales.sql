9207aa1c98ab5423fa26fa058ad96b03ad84b7c9
g.baroyan <g.baroyan@nextcontact.ru>
2023-10-19 18:09:33 +0300

    Незначительны поправки

--- sales/tables/Sales.sql
+++ sales/tables/Sales.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS sales.sales
     sale_id     bigint
         CONSTRAINT pk_sales PRIMARY KEY,
     card_id     integer,
-    employee_id integer      NOT NULL,
+    employee_id integer       NOT NULL,
     shop_id     smallint      NOT NULL,
     sale_info   jsonb         NOT NULL,--nm_id, quantity, product_price
     total_price numeric(8, 2) NOT NULL,
