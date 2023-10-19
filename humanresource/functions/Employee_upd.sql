ea53203ad0f834eac16191c8208db9c3ca316703
g.baroyan <g.baroyan@nextcontact.ru>
2023-10-19 14:22:54 +0300

    Поправил функцию employee_upd + supplier_upd

--- humanresource/functions/Employee_upd.sql
+++ humanresource/functions/Employee_upd.sql
@@ -26,14 +26,14 @@ BEGIN
            s.email,
            s.deleted_at
     INTO _employee_id,
-         _shop_id,
-         _post_id,
-         _first_name,
-         _surname,
-         _patronymic,
-         _phone,
-         _email,
-         _deleted_at
+        _shop_id,
+        _post_id,
+        _first_name,
+        _surname,
+        _patronymic,
+        _phone,
+        _email,
+        _deleted_at
     FROM jsonb_to_record(_src) as s (
                                      employee_id integer,
                                      shop_id integer,
@@ -45,7 +45,43 @@ BEGIN
                                      email varchar(50),
                                      deleted_at TIMESTAMPTZ
         )
-    LEFT JOIN humanresource.employees e ON s.employee_id = e.employee_id;
+             LEFT JOIN humanresource.employees e ON s.employee_id = e.employee_id;
+
+
+    IF _deleted_at IS NOT NULL THEN
+
+        WITH cte_upd AS (
+            UPDATE humanresource.employees e SET
+                deleted_at = _deleted_at
+                WHERE e.employee_id = _employee_id
+                RETURNING e.*)
+
+        INSERT INTO history.employeeschanges AS ec (employee_id,
+                                                     shop_id,
+                                                     post_id,
+                                                     first_name,
+                                                     surname,
+                                                     patronymic,
+                                                     phone,
+                                                     email,
+                                                     deleted_at,
+                                                     ch_staff_id,
+                                                     ch_dt)
+        SELECT ic.employee_id,
+               ic.shop_id,
+               ic.post_id,
+               ic.first_name,
+               ic.surname,
+               ic.patronymic,
+               ic.phone,
+               ic.email,
+               ic.deleted_at,
+               _staff_id,
+               _dt
+        FROM cte_upd ic;
+
+            RETURN JSONB_BUILD_OBJECT('data', NULL);
+    END IF;
 
     WITH ins_cte AS (
         INSERT INTO humanresource.employees AS e (employee_id,
@@ -55,8 +91,7 @@ BEGIN
                                                   surname,
                                                   patronymic,
                                                   phone,
-                                                  email,
-                                                  deleted_at)
+                                                  email)
             SELECT _employee_id,
                    _shop_id,
                    _post_id,
@@ -64,17 +99,15 @@ BEGIN
                    _surname,
                    _patronymic,
                    _phone,
-                   _email,
-                   _deleted_at
+                   _email
             ON CONFLICT (employee_id, shop_id) DO UPDATE
-                SET shop_id    = excluded.shop_id,
-                    post_id    = excluded.post_id,
+                SET shop_id = excluded.shop_id,
+                    post_id = excluded.post_id,
                     first_name = excluded.first_name,
-                    surname    = excluded.surname,
+                    surname = excluded.surname,
                     patronymic = excluded.patronymic,
-                    phone      = excluded.phone,
-                    email      = excluded.email,
-                    deleted_at = excluded.deleted_at
+                    phone = excluded.phone,
+                    email = excluded.email
             RETURNING e.*)
 
     INSERT INTO history.employeeschanges AS ec (employee_id,
@@ -85,7 +118,6 @@ BEGIN
                                                 patronymic,
                                                 phone,
                                                 email,
-                                                deleted_at,
                                                 ch_staff_id,
                                                 ch_dt)
     SELECT ic.employee_id,
@@ -96,7 +128,6 @@ BEGIN
            ic.patronymic,
            ic.phone,
            ic.email,
-           ic.deleted_at,
            _staff_id,
            _dt
     FROM ins_cte ic;
--- supply/functions/Suplier_upd.sql
+++ supply/functions/Suplier_upd.sql
@@ -4,8 +4,61 @@ CREATE OR REPLACE FUNCTION supply.supplier_upd(_src jsonb, _staff_id integer) re
 AS
 $$
 DECLARE
-    _dt timestamptz := now() AT TIME ZONE 'Europe/Moscow';
+    _dt          timestamptz := now() AT TIME ZONE 'Europe/Moscow';
+    _supplier_id integer;
+    _name        varchar(100);
+    _phone       varchar(11);
+    _email       varchar(50);
+    _deleted_at  timestamptz;
 BEGIN
+
+    SELECT COALESCE(s.supplier_id, nextval('supply.supplier_sq')) as supplier_id,
+           s.name,
+           s.phone,
+           s.email,
+           s.deleted_at
+    INTO _supplier_id ,
+        _name ,
+        _phone ,
+        _email ,
+        _deleted_at
+    FROM jsonb_to_record(_src) as s (
+                                     supplier_id integer,
+                                     name varchar(100),
+                                     phone varchar(11),
+                                     email varchar(50),
+                                     deleted_at timestamptz
+        )
+             LEFT JOIN supply.suppliers sp ON sp.supplier_id = s.supplier_id;
+
+
+    IF _deleted_at IS NOT NULL THEN
+
+        WITH cte_upd AS (
+            UPDATE supply.suppliers s SET
+                deleted_at = _deleted_at
+                WHERE s.supplier_id = _supplier_id
+                RETURNING s.*)
+
+
+        INSERT INTO history.supplierschanges AS ec (supplier_id,
+                                                    name,
+                                                    phone,
+                                                    email,
+                                                    deleted_at,
+                                                    ch_staff_id,
+                                                    ch_dt)
+        SELECT ic.supplier_id,
+               ic.name,
+               ic.phone,
+               ic.email,
+               ic.deleted_at,
+               _staff_id,
+               _dt
+        FROM cte_upd ic;
+
+        RETURN JSONB_BUILD_OBJECT('data', NULL);
+    END IF;
 
     WITH ins_cte AS (
         INSERT INTO supply.suppliers AS e (supplier_id,
@@ -14,35 +67,21 @@ BEGIN
                                            email,
                                            deleted_at
             )
-            SELECT COALESCE(s.supplier_id, nextval('supply.supplier_sq')) as supplier_id,
-                   s.name,
-                   s.phone,
-                   s.email,
-                   s.deleted_at
-            FROM jsonb_to_record(_src) as s (
-                                             supplier_id integer,
-                                             name varchar(100),
-                                             phone varchar(11),
-                                             email varchar(50),
-                                             deleted_at timestamptz
-                )
-                     LEFT JOIN supply.suppliers sp ON sp.supplier_id = s.supplier_id
+            SELECT _supplier_id, _name, _phone, _email
             ON CONFLICT (supplier_id) DO UPDATE
-                SET name  = excluded.name,
+                SET name = excluded.name,
                     phone = excluded.phone,
-                    email = excluded.email,
-                    deleted_at = excluded.deleted_at
+                    email = excluded.email
             RETURNING e.*)
 
 
-    INSERT
-    INTO history.supplierschanges AS ec (supplier_id,
-                                         name,
-                                         phone,
-                                         email,
-                                         deleted_at,
-                                         ch_staff_id,
-                                         ch_dt)
+    INSERT INTO history.supplierschanges AS ec (supplier_id,
+                                                name,
+                                                phone,
+                                                email,
+                                                deleted_at,
+                                                ch_staff_id,
+                                                ch_dt)
     SELECT ic.supplier_id,
            ic.name,
            ic.phone,
